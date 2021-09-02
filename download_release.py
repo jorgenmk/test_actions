aee248811d01c755723cff6612d509af3f4e7dd2
Jorgen Kvalvaag <jorgen.kvalvaag@nordicsemi.no>
2021-09-02 22:11:24 +0200

    test

---
+++ download_release.py
@@ -0,0 +1,27 @@
+import requests
+import os
+import io
+import zipfile
+import pprint
+
+url = "https://api.github.com/repos/jorgenmk/test_actions/actions/artifacts"
+headers = {"Accept":"application/json", "Authorization": "token mytoken"}
+
+try:
+	os.mkdir("release")
+except FileExistsError:
+	pass
+
+r = requests.get(url, headers=headers)
+r.raise_for_status()
+data = r.json()
+
+for artifact in data["artifacts"][0:2]:
+	pprint.pprint(artifact)
+	r = requests.get(artifact["archive_download_url"], headers=headers)
+	r.raise_for_status()
+	with zipfile.ZipFile(io.BytesIO(r.content), "r") as z:
+		for fileinfo in z.infolist():
+			with zipfile.ZipFile(io.BytesIO(z.read(fileinfo))) as z2:
+				z2.extractall("release")
+
